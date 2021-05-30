e86d345ba3817786ec006fe35d7536360feeca42
yana-d <yana.dzhulay@dbf-regensburg.de>
2021-05-31 00:07:47 +0200

    manual weekly run. Loading new data from website

--- run_script_broken_no_env.bat
+++ run_script_broken_no_env.bat
@@ -1,2 +1,4 @@
 "C:\Users\Yana\miniconda3\python.exe" "C:\Users\Yana\green-bean-price-tracker\green-bean-price-tracker-script.py"
 pause
+
+#that doesn't work as we skip conda activate green-bean-price-tracker 
